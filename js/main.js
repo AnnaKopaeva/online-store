0f200895d57ae740df6825d83f2b01a994a659ef
Anna <anna.kopaieva@yandex.ua>
2017-05-23 21:56:45 +0300

    Created sections product show close #8 and related products close #9

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
   $('.slider').slick({
-  	dots: true,
-  	arrows: false
+    dots: true,
+    arrows: false
   });
   $('.arrivals-slider').slick({
-  dots: true,
-  arrows: false,
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 4
-});
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
 });
